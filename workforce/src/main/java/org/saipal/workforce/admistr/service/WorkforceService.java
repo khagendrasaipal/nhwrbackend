6e323ccdd2d00463b0b22f64be690846c402ec30
khagendra <khagendra@saipal.org>
2022-05-11 13:25:31 +0545

    excel

--- workforce/src/main/java/org/saipal/workforce/admistr/service/WorkforceService.java
+++ workforce/src/main/java/org/saipal/workforce/admistr/service/WorkforceService.java
@@ -1,28 +1,18 @@
 package org.saipal.workforce.admistr.service;
 
-import java.io.ByteArrayInputStream;
-import java.io.ByteArrayOutputStream;
-import java.io.File;
-import java.io.FileOutputStream;
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 import javax.persistence.Tuple;
+import javax.servlet.ServletOutputStream;
 import javax.servlet.http.HttpServletResponse;
-import javax.transaction.Transactional;
-import org.apache.commons.csv.CSVFormat;
 import org.apache.commons.csv.CSVPrinter;
 import org.apache.poi.ss.usermodel.Cell;
 import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.ss.usermodel.Sheet;
 import org.apache.poi.ss.usermodel.Workbook;
-import org.apache.poi.xssf.usermodel.XSSFCell;
-import org.apache.poi.xssf.usermodel.XSSFRow;
-import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.saipal.fmisutil.auth.Authenticated;
 import org.saipal.fmisutil.service.AutoService;
@@ -32,7 +22,6 @@ import org.saipal.fmisutil.util.Messenger;
 import org.saipal.fmisutil.util.Paginator;
 import org.saipal.workforce.admistr.model.Workforce;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 @Component
@@ -94,7 +83,6 @@ public class WorkforceService extends AutoService {
 		model.loadData(document);
 		String sql1="select * from tbl_workforce where org=? and org!=?";
 		List<Tuple> ofcstr = db.getResultList(sql1, Arrays.asList(model.org,0));
-//		System.out.println(ofcstr.size());
 		if (ofcstr.isEmpty()) {
 			sql = "INSERT INTO tbl_workforce (id,orgtype,provinceid,districtid,palika,ward,org,orgname,authority,authlevel,ownership,ftype,created_by) VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?)";
 			DbResponse rowEffect = db.execute(sql,
@@ -105,7 +93,6 @@ public class WorkforceService extends AutoService {
 					if("".equals(request("groupid"+i))) {
 						
 					}else {
-//						System.out.println(request("groupid"+i));
 						String did=db.newIdInt();
 						String sql2="INSERT INTO tbl_darbandi(id,workforce_id,orgid,groupid,subgroupid,post,post_no,post_no_karar) values(?,?,?,?,?,?,?,?)";
 						DbResponse rowEffect2 = db.execute(sql2,
@@ -122,7 +109,7 @@ public class WorkforceService extends AutoService {
 						}
 					}
 				}
-				return Messenger.getMessenger().success();
+				return Messenger.getMessenger().setData(pid).success();
 
 			} else {
 				return Messenger.getMessenger().error();
@@ -172,70 +159,107 @@ public class WorkforceService extends AutoService {
 	}
 
 
-	public ByteArrayInputStream getcsv(HttpServletResponse response) {
+	public void getcsv(HttpServletResponse response) throws IOException {
 		String id=request("id");
 		String filename = "healthworkforce.csv";
-		String sql = "select hfregistry.hf_name,tbl_workforce.orgname,tbl_samuha.namenp as groupname,tbl_upasamuha.namenp as subgroup,tbl_post.namenp as postname,tbl_employee.namenp as empname from tbl_darbandi_details as td"
+		String sql = "select COALESCE(hfregistry.hf_name,'') as hf_name,COALESCE(tbl_level.namenp,'') as level,COALESCE(tbl_emptype.namenp,'') as emptype,COALESCE(tbl_council.namenp,'') as council,COALESCE(tbl_workforce.orgname,'')as orgname,tbl_samuha.namenp as groupname,COALESCE(tbl_upasamuha.namenp,'') as subgroup,tbl_post.namenp as postname,COALESCE(tbl_employee.namenp,'') as empname,"
+				+ " COALESCE(tbl_employee.address,'') as address,COALESCE(tbl_employee.email,'') as email,COALESCE(tbl_employee.mobile,'') as mobile,COALESCE(tbl_employee.apoint_date,'') as apoint_date,COALESCE(tbl_employee.att_date,'') as att_date,COALESCE(tbl_employee.council_no,'') as council_no from tbl_darbandi_details as td"
 				+ " left join hfregistry on hfregistry.id=td.orgid join tbl_workforce on tbl_workforce.id=td.workforce_id join tbl_samuha on tbl_samuha.id=td.groupid left join tbl_upasamuha on "
-				+ " tbl_upasamuha.id=td.subgroupid join tbl_post on tbl_post.id=td.post left join tbl_employee on tbl_employee.detailsid=td.id where td.workforce_id=?";
-//		System.out.println(sql);
+				+ " tbl_upasamuha.id=td.subgroupid join tbl_post on tbl_post.id=td.post  left join tbl_employee on tbl_employee.detailsid=td.id left join tbl_emptype on tbl_emptype.id=tbl_employee.emptype left join tbl_level on tbl_level.id=tbl_employee.level left join tbl_council on tbl_council.id=tbl_employee.council where td.workforce_id=?";
+
 		List<Tuple> admlvl = db.getResultList(sql, Arrays.asList(id));
 		
-		response.setContentType("application/octet-stream");
-        response.setHeader("Content-Disposition", "attachment; filename=contacts.xlsx");
-		
-//		String TYPE = "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet";
-//		  String[] HEADERs = { "Id", "Title", "Description", "Published" };
-//		  String SHEET = "Tutorials";
-		
-		  try(Workbook workbook = new XSSFWorkbook(); ByteArrayOutputStream out = new ByteArrayOutputStream();) {
+		Workbook wb = new XSSFWorkbook();
+		Sheet sheet = wb.createSheet("records");
 
-	            
-		      Sheet sheet = workbook.createSheet("contacts.xlsx");
-		      // Header
-		      Row headerRow = sheet.createRow(0);
-		      for (int col = 0; col < HEADERs.length; col++) {
-		        Cell cell = headerRow.createCell(col);
-		        cell.setCellValue(HEADERs[col]);
-		      }
-		      int rowIdx = 1;
-		      int i = 2;
-		      for (Tuple t : admlvl) {
-		    	  Row row = sheet.createRow(rowIdx++);
-		          row.createCell(0).setCellValue(t.get("groupname")+"");
-		          row.createCell(1).setCellValue(t.get("groupname")+"");
-		          row.createCell(2).setCellValue(t.get("groupname")+"");
-		          row.createCell(3).setCellValue(t.get("groupname")+"");
-		      }
-		      workbook.write(out);
-		      System.out.println("yaha xcu ma");
-		      return new ByteArrayInputStream(out.toByteArray());
-		     
-		    } catch (IOException e) {
-		    	System.out.println(e.getMessage());
-		      throw new RuntimeException("fail to import data to Excel file: " + e.getMessage());
-		    }
+		int rowCount = 0;
+		Row row = sheet.createRow(rowCount++);
 		
-//		response.setContentType("text/csv");
-//		response.setHeader(HttpHeaders.CONTENT_DISPOSITION,
-//				"attachment; filename=\"" + filename + "\"");
-//		
-//		 try {
-//			csvPrinter = new CSVPrinter(response.getWriter(),
-//					CSVFormat.EXCEL.withHeader("HF name", "orgname", "Group","subgroup","employee"));
-//			if (!admlvl.isEmpty()) {
-//				for (Tuple t : admlvl) {
-//					csvPrinter.printRecord(Arrays.asList(t.get("hf_name"),t.get("orgname"),t.get("groupname"),t.get("subgroup"),t.get("empname")));
-//				}
-//				
-//			
-//		}
-//		}catch (IOException e) {
-//			e.printStackTrace();
-//		} finally {
-//			if(csvPrinter != null)
-//				csvPrinter.close();
-//		}
+		Cell cell = row.createCell(0);
+		cell.setCellValue("Hf Name");
+		
+		cell = row.createCell(1);
+		cell.setCellValue("Orgname");
+
+		cell = row.createCell(2);
+		cell.setCellValue("Group");
+
+		cell = row.createCell(3);
+		cell.setCellValue("Subgroup");
+		
+		cell = row.createCell(4);
+		cell.setCellValue("Employename");
+		
+		cell = row.createCell(5);
+		cell.setCellValue("Address");
+		
+		cell = row.createCell(6);
+		cell.setCellValue("email");
+
+		cell = row.createCell(7);
+		cell.setCellValue("Mobile");
+		
+		cell = row.createCell(8);
+		cell.setCellValue("Appoint Date");
+		
+		cell = row.createCell(9);
+		cell.setCellValue("Attend Date");
+		
+		cell = row.createCell(10);
+		cell.setCellValue("Level");
+		
+		cell = row.createCell(11);
+		cell.setCellValue("Council");
+		
+		cell = row.createCell(12);
+		cell.setCellValue("Council no");
+		
+		cell = row.createCell(13);
+		cell.setCellValue("Emp Type");
+
+			for (Tuple t : admlvl) {
+				row = sheet.createRow(rowCount++);
+
+				int columnCount = 0;
+				cell = row.createCell(columnCount++);
+				cell.setCellValue(t.get("hf_name")+"");
+				cell = row.createCell(columnCount++);
+				cell.setCellValue(t.get("orgname")+"");
+				cell = row.createCell(columnCount++);
+				cell.setCellValue(t.get("groupname")+"");
+				cell = row.createCell(columnCount++);
+				cell.setCellValue(t.get("subgroup")+"");
+				cell = row.createCell(columnCount++);
+				cell.setCellValue(t.get("empname")+" ");
+				cell = row.createCell(columnCount++);
+				cell.setCellValue(t.get("address")+"");
+				cell = row.createCell(columnCount++);
+				cell.setCellValue(t.get("email")+"");
+				cell = row.createCell(columnCount++);
+				cell.setCellValue(t.get("mobile")+"");
+				cell = row.createCell(columnCount++);
+				cell.setCellValue(t.get("apoint_date")+"");
+				cell = row.createCell(columnCount++);
+				cell.setCellValue(t.get("att_date")+"");
+				cell = row.createCell(columnCount++);
+				cell.setCellValue(t.get("level")+"");
+				cell = row.createCell(columnCount++);
+				cell.setCellValue(t.get("council")+"");
+				cell = row.createCell(columnCount++);
+				cell.setCellValue(t.get("council_no")+"");
+				cell = row.createCell(columnCount++);
+				cell.setCellValue(t.get("emptype")+"");
+			}
+			String headerKey = "Content-Disposition";
+			String headerValue = "attachment; filename=records.xlsx";
+			response.setHeader(headerKey, headerValue);
+			
+			ServletOutputStream outputStream = response.getOutputStream();
+			wb.write(outputStream);
+			wb.close();
+
+			outputStream.close();
+
 		
 }
 }
